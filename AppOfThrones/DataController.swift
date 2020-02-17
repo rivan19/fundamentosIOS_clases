f3ede1af98312e2fb6f31e76db222644c7f4297a
Ivan Llopis <rivan@me.com>
2020-02-18 00:28:17 +0100

    Inicio patrón Singleton. DataController.swift. Fin de Clase

---
+++ AppOfThrones/DataController.swift
@@ -0,0 +1,20 @@
+//
+//  DataController.swift
+//  AppOfThrones
+//
+//  Created by Ivan Llopis Guardado on 17/02/2020.
+//  Copyright © 2020 Ivan Llopis Guardado. All rights reserved.
+//
+
+import Foundation
+
+class DataController {
+    
+    //var rating: [Rating] = []
+    
+    static var shared = DataController()
+    private init() {}
+    
+    var id: Int = 0
+    
+}
